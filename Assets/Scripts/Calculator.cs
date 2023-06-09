e60e4b96c117cd0f5032bac3bed89cf43bbfb5d3
Veteread <veteran23@mail.ru>
2023-06-09 17:05:42 +0300

    4

---
+++ Assets/Scripts/Calculator.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Calculator : MonoBehaviour
+{
+	public InputField firstNomber;
+    public InputField secondNomber;
+    public Text result;
+  
+	private float a;
+    private float b;
+    private float c;
+
+    private void check()
+    {
+        
+        if (firstNomber.text == "" || secondNomber.text == "")
+        {
+            result.text = "Введите число";
+        }
+    }
+      public void onPressPlus()
+    {
+        check();
+        a = float.Parse(firstNomber.text);
+        b = float.Parse(secondNomber.text);
+        c = a + b;
+        result.text = $"{c}";
+
+    }
+    public void onPressMinus()
+    {
+        check(); 
+        a = float.Parse(firstNomber.text);
+        b = float.Parse(secondNomber.text);
+        c = a - b;
+        result.text = $"{c}";
+
+    }
+    public void onPressMultiplication()
+    {
+        check();
+        a = float.Parse(firstNomber.text);
+        b = float.Parse(secondNomber.text);
+        c = a * b;
+        result.text = $"{c}";
+    }
+    public void onPressDevision()
+    {
+        check();
+        a = float.Parse(firstNomber.text);
+        b = float.Parse(secondNomber.text);
+        c = a / b;
+        result.text = $"{c}";
+    }
+    public void onPressClear()
+    {
+    	a = 0;
+    	b = 0;
+    	c = 0;
+    	firstNomber.text = "";
+    	secondNomber.text = "";
+    	result.text = "Результат";
+    }
+
+   
+}
---
+++ Assets/Scripts/TwoNumbersComparer.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TwoNumbersComparer : MonoBehaviour
+{
+    public InputField nomber1;
+    public InputField nomber2;
+    public Text resultCom;
+    private float a;
+    private float b;
+   
+    public void onPressCom()
+    {
+        if (nomber1.text == "" || nomber2.text == "")
+    	{
+    		resultCom.text = "Введите число";    		   		
+    	}
+    	else 
+    	{ 
+    	a = float.Parse(nomber1.text);
+    	b = float.Parse(nomber2.text);
+    	 if (a > b)
+    	 {
+    	 	resultCom.text = $"Наибольшее число {a}";
+    	 }
+    	 else
+    	  {
+    	  	if (a < b)
+    	  	{
+    	  		resultCom.text = $"Наибольшее число {b}";    	  		
+    	  	}
+    	  	else 
+    	  	 if (a == b)
+    	  	 {
+    	  	 	resultCom.text = "Числа равны";
+    	  	 }
+    	  }
+    	}
+
+    }
+
+    public void onPressClearCom()
+    {
+    	a = 0;
+    	b = 0;
+    	nomber1.text = "";
+    	nomber2.text = "";
+    	resultCom.text = "";
+    }
+}
